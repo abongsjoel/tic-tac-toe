d4c64fdc379d705fff3142cd3d9d40e61736da40
tj-ubuntu <abongsjoel@gmail.com>
2021-01-28 03:09:22 +0100

    Implement functions play_game and move

--- bin/main.rb
+++ bin/main.rb
@@ -2,11 +2,16 @@
 
 def tic_tac_toe
   intro
-  player_x = get_player("first")
-  player_o = get_player("second")
+  player_x = get_player('first')
+  player_o = get_player('second')
   puts "\nNow lets start playing"
   starter = start_bid(player_x, player_o)
   puts "\n#{starter} wins bid to start"
+  if (starter == player_x)
+    play_game(player_x, player_o)
+  else
+    play_game(player_o, player_x)
+  end
 end
 
 def intro
@@ -30,6 +35,18 @@ def start_bid(player_x, player_o)
   else
     player_o
   end
+end
+
+def play_game(first, second)
+  move(first)
+  move(second)
+end
+
+def move(player)
+  puts "\n #{player} make a move"
+  puts "\n You can select a positive integer between 1 and 9"
+  value = gets.chomp
+  puts "\n Okay #{player}, now your move is displayed on the board"
 end
 
 tic_tac_toe
