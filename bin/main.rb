4005e7f7d6f0d1cc62d2f1d6bfd518c0a36773be
tj-ubuntu <abongsjoel@gmail.com>
2021-01-28 02:04:36 +0100

    Create introduction function

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,16 @@
 #!/usr/bin/env ruby
 
-puts 'Hello World!'
+def tic_tac_toe
+  intro
+  puts "Name of first Player"
+end
+
+def intro
+  puts "Hello and welcome to the world TIC-TAC-TOE\n\n"
+  puts " 1 | 2 | 3 \n --+---+--"
+  puts " 4 | 5 | 6 \n --+---+--"
+  puts " 7 | 8 | 9 \n --+---+--"
+  puts "\nType in your names to start this awesome game\n\n"
+end
+
+tic_tac_toe
