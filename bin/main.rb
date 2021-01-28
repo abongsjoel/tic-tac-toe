61dd527349be1c0f592baaa9185086c1dd55ca16
tj-ubuntu <abongsjoel@gmail.com>
2021-01-28 03:16:47 +0100

    Remove numbers from board in intro

--- bin/main.rb
+++ bin/main.rb
@@ -16,9 +16,9 @@ end
 
 def intro
   puts "Hello and welcome to the world TIC-TAC-TOE\n\n"
-  puts " 1 | 2 | 3 \n --+---+--"
-  puts " 4 | 5 | 6 \n --+---+--"
-  puts " 7 | 8 | 9 \n --+---+--"
+  puts "   |   |   \n --+---+--"
+  puts "   |   |   \n --+---+--"
+  puts "   |   |   \n "
   puts "\nType in your names to start this awesome game\n"
 end
 
