28f8cc3cd400812eaac47d87093b7311ee6e0d4f
tj-ubuntu <abongsjoel@gmail.com>
2021-01-28 21:59:27 +0100

    Fix message to ask for an available move

--- bin/main.rb
+++ bin/main.rb
@@ -58,7 +58,7 @@ def make_move(player)
   puts "\n You can select a positive integer between 1 and 9"
   value = gets.chomp.to_i
   until value >= 1 && value <= 9
-    puts "\nError! Please select a positive integer between 1 and 9"
+    puts "\nError! Please select a positive integer between 1 and 9 that has not been selected yet"
     value = gets.chomp.to_i
   end
   puts "\n Okay #{player}, now your move is displayed on the board: #{value}"
