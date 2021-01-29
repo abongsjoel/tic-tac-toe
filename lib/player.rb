1647daa8a25f0c416353475e411d3c181e9ca9a8
tj-ubuntu <abongsjoel@gmail.com>
2021-01-29 22:50:50 +0100

    Display available moves

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,6 @@
 class Player
   attr_accessor :name
-  @@available_moves = [0..9]
+  @@available_moves = *(0..9)
   def initialize(name, mark)
     @name = name
     @mark = mark
@@ -11,20 +11,22 @@ class Player
 
   def make_move(board)
     puts "\n #{self.name} make a move"
-    puts "\n You can select a positive integer between 1 and 9\n"
+    # puts "\n You can select a positive integer between 1 and 9\n"
+    puts "\nAvailable moves: #{@@available_moves.join(', ')}\n"
     value = gets.chomp.to_i
     until value >= 1 && value <= 9
       puts "\nError! Please select a positive integer between 1 and 9 that has not been selected yet"
       value = gets.chomp.to_i
     end
+    puts "\n"
     update_board(value, board)
   end
 
   def check_win_or_draw
     # win = rand(0..9).eql?(5) ? true : false
     # draw = rand(0..8).eql?(4) ? true : false
-    win = true
-    draw = true
+    win = false
+    draw = false
     if win
       puts "\nCongratzzz #{self.name}, you win. Lets pop the champagne"
       false
@@ -39,7 +41,6 @@ class Player
   private
 
   def update_board(value, board)
-    p "Update value is #{@positions[value]}"
     board.cell[@positions[value][0]][@positions[value][1]] = @mark
     board.display
   end
