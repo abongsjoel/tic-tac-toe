c3dfae0020043c64fa6dbfaf2135030fda3f087e
tj-ubuntu <abongsjoel@gmail.com>
2021-01-29 21:50:51 +0100

    Initialize positions variable in Player class

--- lib/player.rb
+++ lib/player.rb
@@ -3,6 +3,9 @@ class Player
   def initialize(name, mark)
     @name = name
     @mark = mark
+    @positions = { 1 => [0, 0], 2 => [0, 1], 3 => [0, 2],
+                   4 => [1, 0], 5 => [1, 1], 6 => [1, 2],
+                   7 => [2, 0], 8 => [2, 1], 9 => [2, 2] }
   end
 
   def make_move
@@ -13,13 +16,13 @@ class Player
       puts "\nError! Please select a positive integer between 1 and 9 that has not been selected yet"
       value = gets.chomp.to_i
     end
-    puts "\n Okay #{self.name}, now your move is displayed on the board: #{value}"
+    # puts "\n Okay #{self.name}, now your move is displayed on the board: #{value}"
     update_board(value)
   end
 
   private
 
   def update_board(value)
-    p "Update value is #{value}"
+    p "Update value is #{@positions[value]}"
   end
 end
