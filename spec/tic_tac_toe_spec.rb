2e61b4c0d869465be4817a7be4f55e97a4f2b356
tj-ubuntu <abongsjoel@gmail.com>
2021-02-16 00:00:44 +0100

    Edit test for make_move method

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -7,11 +7,11 @@ describe Player do
   let(:board) { Board.new }
 
   describe "#make_move" do
-    it "returns a board with player mark at right position" do
-      expect(player1.make_move(board, 3)).to include('   |   | X ')
+    it "returns a board with player X mark at right position" do
+      expect(player1.make_move(board, 3)).to eql("   |   | X  \n --+---+--\n   |   |    \n --+---+--\n   |   |    \n ")
     end
-    it "returns a board with player mark at right position" do
-      expect(player2.make_move(board, 1)).to include(' O |   |  ')
+    it "returns a board with player O mark at right position" do
+      expect(player2.make_move(board, 9)).to eql("   |   |    \n --+---+--\n   |   |    \n --+---+--\n   |   | O  \n ")
     end
   end
 
