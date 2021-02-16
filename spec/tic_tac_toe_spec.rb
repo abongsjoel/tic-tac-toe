833b413cad49b4796dd47d50bdf9a24db7f90cdc
tj-ubuntu <abongsjoel@gmail.com>
2021-02-16 22:38:41 +0100

    Fix linter issues

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -6,56 +6,55 @@ describe Player do
   let(:player2) { Player.new('Joel', 'O') }
   let(:board) { Board.new }
 
-  describe "#make_move" do
-    it "returns a board with player X mark at right position" do
+  describe '#make_move' do
+    it 'returns a board with player X mark at right position' do
       expect(player1.make_move(board, 3)).to eql("   |   | X  \n --+---+--\n   |   |    \n --+---+--\n   |   |    \n ")
     end
-    it "returns a board with player O mark at right position" do
+    it 'returns a board with player O mark at right position' do
       expect(player2.make_move(board, 9)).to eql("   |   |    \n --+---+--\n   |   |    \n --+---+--\n   |   | O  \n ")
     end
   end
 
-  describe "#check_win" do
-    it "returns true if player wins" do
-      player1.collection = [4,5,6]
+  describe '#check_win' do
+    it 'returns true if player wins' do
+      player1.collection = [4, 5, 6]
       expect(player1.check_win).to eql(true)
     end
-    it "returns false if player does not win" do
-      player1.collection = [4,5]
+    it 'returns false if player does not win' do
+      player1.collection = [4, 5]
       expect(player1.check_win).to eql(false)
     end
   end
 end
 
 describe Board do
-  let(:board) {Board.new}
-  describe "#display" do
-    it "returns an empty board" do
+  let(:board) { Board.new }
+  describe '#display' do
+    it 'returns an empty board' do
       expect(board.display).to eql("   |   |    \n --+---+--\n   |   |    \n --+---+--\n   |   |    \n ")
     end
   end
 
-  describe "#display_availble_moves" do
-    it "returns available moves" do
-      expect(board.display_availble_moves).to eql("1, 2, 3, 4, 5, 6, 7, 8, 9")
+  describe '#display_availble_moves' do
+    it 'returns available moves' do
+      expect(board.display_availble_moves).to eql('1, 2, 3, 4, 5, 6, 7, 8, 9')
     end
   end
 
-  describe "#check_draw" do
-    it "returns true if available moves is empty" do
+  describe '#check_draw' do
+    it 'returns true if available moves is empty' do
       board.available_moves = []
       expect(board.check_draw).to eql(true)
     end
 
-    it "returns false if available moves is not empty" do
+    it 'returns false if available moves is not empty' do
       expect(board.check_draw).to eql(false)
     end
   end
 
-  describe "#update_availble_moves" do
-    it "return array without value pass in" do
+  describe '#update_availble_moves' do
+    it 'return array without value pass in' do
       expect(board.update_availble_moves(3)).to_not include(3)
     end
   end
-
 end
